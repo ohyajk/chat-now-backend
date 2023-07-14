c0945aca00df29096b9cb9fe25ff444f978c0d2c
ohyajk <ohyajk@gmail.com>
2023-07-14 15:51:19 +0530

    Add Store in mongo store session

--- App/Middlewares/sessionMiddleware.js
+++ App/Middlewares/sessionMiddleware.js
@@ -1,5 +1,5 @@
 const session = require('express-session');
-
+const MongoDBStore = require('connect-mongodb-session')(session);
 const sessionMiddleware = session({
     name: 'sid',
     secret: 'my-secret-key',
@@ -7,7 +7,11 @@ const sessionMiddleware = session({
     saveUninitialized: true,
     cookie: {
         maxAge: 1000 * 60 * 60,
-    }
+    },
+    store: new MongoDBStore({
+        uri: process.env.DB_URL,
+        collection: 'sessions'
+    })
 });
 
 module.exports = sessionMiddleware;
